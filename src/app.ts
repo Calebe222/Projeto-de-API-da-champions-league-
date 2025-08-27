c0087fae9906ebbeb60596ecf85cf77c48af3ea5
Calebe de Freitas <caiodefreitas16@gmail.com>
2025-08-27 11:02:26 -0400

    Lançamento do projeto

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+import express from "express"
+import router from "./routes";
+import cors from "cors"
+
+
+function createApp() {
+  const app = express()
+
+app.use(express.json())
+app.use("/api", router)
+
+const corsOptions = {
+  origin: ["http://felipao.sistem.com", "http://gov.br"],
+  methods: ["GET"]
+}
+
+app.use(cors())
+
+
+  return app
+}
+
+
+export default createApp;
